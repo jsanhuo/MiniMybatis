500ed8598a0756349633406fd2cc01ab9a663812
jiaoyan <1483104508@qq.com>
2018-11-13 07:34:38 +0800

    FactoryBuilder读取配置创建Factory基本构造完成

--- sqlfactory/MySqlSessionFactoryBuilder.java
+++ sqlfactory/MySqlSessionFactoryBuilder.java
@@ -1,6 +1,14 @@
 package cn.chenmixuexi.sqlfactory;
 
+import cn.chenmixuexi.datasource.DefaultDataSource;
+import cn.chenmixuexi.parsexml.ParseXml;
+import cn.chenmixuexi.parsexml.bean.XmlBean;
+
+import javax.sql.DataSource;
+import java.io.File;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MySqlSessionFactoryBuilder {
     /**
@@ -10,7 +18,46 @@ public class MySqlSessionFactoryBuilder {
      * @param in
      * @return
      */
-    public MySqlSessionFactory build(InputStream in){
-        return null;
+    public MySqlSessionFactory build(InputStream in) throws Exception {
+        XmlBean xmlBean = ParseXml.ParseXml(in);
+        HashMap<String, String> environment = xmlBean.getEnvironment();
+        HashMap<String, ArrayList<String>> mappers = xmlBean.getMappers();
+        if(environment==null&&mappers==null){
+            throw new Exception("配置文件出错,加载MySqlSessionFactory出错");
+        }
+        if(environment!=null&&mappers!=null){
+            DefaultDataSource defaultDataSource = new DefaultDataSource(environment.get("driver"),environment.get("url"),environment.get("username"),environment.get("password"));
+            return new MySqlSessionFactory(mappers.get("class"),mappers.get("resource"),defaultDataSource);
+        }else{
+            throw new Exception("配置文件出错,加载MySqlSessionFactory出错");
+        }
+    }
+
+    public MySqlSessionFactory build(File in) throws Exception {
+        XmlBean xmlBean = ParseXml.ParseXml(in);
+        HashMap<String, String> environment = xmlBean.getEnvironment();
+        HashMap<String, ArrayList<String>> mappers = xmlBean.getMappers();
+        if(environment==null&&mappers==null){
+            throw new Exception("配置文件出错,加载MySqlSessionFactory出错");
+        }
+        if(environment!=null&&mappers!=null){
+            DefaultDataSource defaultDataSource = new DefaultDataSource(environment.get("driver"),environment.get("url"),environment.get("username"),environment.get("password"));
+            return new MySqlSessionFactory(mappers.get("class"),mappers.get("resource"),defaultDataSource);
+        }else{
+            throw new Exception("配置文件出错,加载MySqlSessionFactory出错");
+        }
+    }
+
+
+
+    public MySqlSessionFactory build(InputStream in, DataSource dataSource) throws Exception {
+        XmlBean xmlBean = ParseXml.ParseXml(in);
+        HashMap<String, String> environment = xmlBean.getEnvironment();
+        HashMap<String, ArrayList<String>> mappers = xmlBean.getMappers();
+        if(mappers==null){
+            throw new Exception("配置文件出错,加载MySqlSessionFactory出错");
+        }else {
+            return new MySqlSessionFactory(mappers.get("class"),mappers.get("resource"),dataSource);
+        }
     }
 }
