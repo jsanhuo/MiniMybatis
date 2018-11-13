3214bc77a52ca86a4a2276f139e2356390c87678
jiaoyan <1483104508@qq.com>
2018-11-13 14:41:04 +0800

    SqlSession已经基本完成

--- sqlfactory/MySqlSession.java
+++ sqlfactory/MySqlSession.java
@@ -1,27 +1,54 @@
 package cn.chenmixuexi.sqlfactory;
 
+import cn.chenmixuexi.Proxy.MapperInvocationHandler;
+import cn.chenmixuexi.Proxy.bean.CacheBean;
+
+import java.lang.reflect.Proxy;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class MySqlSession {
-    private ConcurrentHashMap<String,Object> firstLevelCache;
+    private ConcurrentHashMap<CacheBean,Object> firstLevelCache;
+    private Connection connection;
+
+    public MySqlSession(Connection connection) {
+        this.connection = connection;
+        firstLevelCache = new ConcurrentHashMap<CacheBean,Object>();
+    }
     /**
      * 提交事务
      * @throws SQLException
      */
-    public void commit() throws SQLException {
+    public void commit() {
+        try {
+            connection.commit();
+        } catch (SQLException e) {
+            System.err.println("提交失败");
+        }
     }
 
-    public void roolback(){
+    public void roolback()  {
+        try {
+            connection.rollback();
+        } catch (SQLException e) {
+            System.err.println("回滚失败");
+        }
     }
     /**
      * 关闭连接
      */
     public void close(){
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            System.err.println("关闭连接失败");
+        }
     }
-
-
     public <T> T getMapper(Class<T> mapp){
-        return null;
+        if(!mapp.isInterface()){
+            throw new IllegalArgumentException(mapp+"is not an interface");
+        }
+        return (T)Proxy.newProxyInstance(mapp.getClassLoader(),new Class[]{mapp},new MapperInvocationHandler(connection,firstLevelCache));
     }
 }
