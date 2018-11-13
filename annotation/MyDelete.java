fe0c44ddadacd6ae4d69f882303c66ceb9432213
jiaoyan <1483104508@qq.com>
2018-11-13 13:16:26 +0800

    添加注解的类型

--- annotation/MyDelete.java
+++ annotation/MyDelete.java
@@ -1,5 +1,12 @@
 package cn.chenmixuexi.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface MyDelete {
     String value();
 }
