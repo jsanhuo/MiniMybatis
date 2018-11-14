bd0d6255bd8ef3d3943e5bbc6329b95c0bfb5436
jiaoyan <1483104508@qq.com>
2018-11-15 00:02:41 +0800

    添加注解的注释

--- annotation/MyDelete.java
+++ annotation/MyDelete.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 删除Sql注解
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MyDelete {
